2009cfefbae34b7e481db160b6c86bda0139454b
nich-sugiarto <nicholas.sugiarto@gmail.com>
2023-01-25 12:14:05 -0500

    Created new pipeline for GO terms

---
+++ metascapeProcessing.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+
+# Takes in a csv of metascape output (formatted the same way as the one that Luke gave me)
+# Makes a corresponding GO plot of the data, and for each entry description, plots the log fold change and 
+# adjusted p value.
+
